f1749ca6bda2ada9b59e0104c1f8cf6cd5bc8e40
r0manb <romanse.b27@gmail.com>
2025-05-01 00:17:21 +0500

    Add example_parser.py with ExampleParser class

---
+++ parsers/example_parser.py
@@ -0,0 +1,45 @@
+import datetime as dt
+
+import pandas as pd
+import requests
+
+from .data_parser import DataParser
+
+
+class ExampleParser(DataParser):
+    def __init__(
+        self,
+        ticker: str,
+        interval: str,
+        start: dt.datetime = None,
+        end: dt.datetime = None,
+    ):
+        super().__init__(ticker, start, end)
+        self.interval = interval
+
+    def fetch_data(self):
+        response = requests.get(
+            "https://www.example.ru/api/trading/candles",
+            {
+                "start": self.start.isoformat(),
+                "end": self.end.isoformat(),
+                "ticker": self.ticker,
+                "interval": self.interval,
+            },
+        )
+        data = response.json()["payload"]["candles"]
+        procesed_data = self._normalize_data(data)
+
+        self._data = procesed_data
+        return procesed_data
+
+    def _normalize_data(self, data):
+        df = pd.DataFrame(data)
+
+        processed_data = df[["c", "date"]].copy()
+        processed_data.columns = ["close", "date"]
+
+        processed_data["date"] = pd.to_datetime(processed_data["date"], unit="s")
+        processed_data.set_index("date", inplace=True)
+
+        return processed_data
