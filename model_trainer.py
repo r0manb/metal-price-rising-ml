c70d7f61e92407c548eeb065c1edc3dbc717a8b8
r0manb <romanse.b27@gmail.com>
2025-05-05 22:56:10 +0500

    refactor: rename attributes model/scaler to protected _model/_scaler and optimize window creation in ModelTrainer

--- model_trainer.py
+++ model_trainer.py
@@ -4,7 +4,6 @@ import pathlib
 from typing import Optional, Union
 
 import numpy as np
-import pandas as pd
 import pickle
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
@@ -24,7 +23,7 @@ class ModelTrainer:
         model_path: Union[str, os.PathLike] = "/",
     ):
         self.data_parser = data_parser
-        self.scaler = MinMaxScaler((0, 1))
+        self._scaler = MinMaxScaler((0, 1))
 
         self.epochs = epochs
         self.train_percent = train_percent
@@ -60,49 +59,34 @@ class ModelTrainer:
 
     def _initialize_scaler(self):
         train_data = self.data.iloc[: self.train_len + self.window_size]
-        self.scaler.fit(train_data[["close"]].values)
+        self._scaler.fit(train_data[["close"]].values)
 
     def _preprocess_data(self) -> None:
-        prices = self.scaler.transform(self.data[["close"]].values)
+        prices = self._scaler.transform(self.data[["close"]].values)
         dates = self.data.index.values
 
         X = []
         y = []
-        window_dates = []
         for i in range(len(self.data) - self.window_size):
             X.append(prices[i : i + self.window_size])
             y.append(prices[i + self.window_size])
-            window_dates.append(dates[i + self.window_size])
 
-        windows = pd.DataFrame({})
-        windows["date"] = window_dates
-
-        X = np.array(X)
-        for i in range(self.window_size):
-            windows[f"day - {self.window_size - i}"] = X[:, i]
-        windows["target"] = y
-
-        self.preprocessed_data = windows
+        self._X = np.array(X).reshape(-1, self.window_size, 1).astype(np.float32)
+        self._y = np.array(y).astype(np.float32)
+        self._dates = dates[self.window_size :]
 
     def _prepare_data(self) -> None:
-        data = self.preprocessed_data.to_numpy()
+        self.train_X = self._X[: self.train_len]
+        self.train_y = self._y[: self.train_len]
+        self.train_dates = self._dates[: self.train_len]
 
-        X = data[:, 1:-1]
-        X = X.reshape(*X.shape, 1).astype(np.float32)
-        y = data[:, -1].astype(np.float32)
-        dates = data[:, 0]
+        self.val_X = self._X[self.train_len : self.val_len]
+        self.val_y = self._y[self.train_len : self.val_len]
+        self.val_dates = self._dates[self.train_len : self.val_len]
 
-        self.train_X = X[: self.train_len]
-        self.train_y = y[: self.train_len]
-        self.train_dates = dates[: self.train_len]
-
-        self.val_X = X[self.train_len : self.val_len]
-        self.val_y = y[self.train_len : self.val_len]
-        self.val_dates = dates[self.train_len : self.val_len]
-
-        self.test_X = X[self.val_len :]
-        self.test_y = y[self.val_len :]
-        self.test_dates = dates[self.val_len :]
+        self.test_X = self._X[self.val_len :]
+        self.test_y = self._y[self.val_len :]
+        self.test_dates = self._dates[self.val_len :]
 
     def _initialize_model(self):
         model = Sequential(
@@ -123,19 +107,19 @@ class ModelTrainer:
         )
 
         model.compile(optimizer="adam", loss="mse", metrics=["mean_absolute_error"])
-        self.model = model
+        self._model = model
 
     def _save_models(self, path: pathlib.Path) -> None:
-        self.model.save(path / "best_model.keras")
+        self._model.save(path / "best_model.keras")
         with open(path / "best_scaler.pkl", "wb") as f:
-            pickle.dump(self.scaler, f)
+            pickle.dump(self._scaler, f)
 
     def _save_config(self, path: pathlib.Path) -> None:
         with open(path / "config.json", "w", encoding="utf-8") as f:
             json.dump({"window_size": self.window_size}, f)
 
     def train(self) -> None:
-        self.model.fit(
+        self._model.fit(
             self.train_X,
             self.train_y,
             validation_data=(self.val_X, self.val_y),
